859cb00ebe41381235077e309333ea6d40ce9d5e
yysddjy <87555035+yysddjy@users.noreply.github.com>
2021-07-18 15:23:59 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    alert("Hello JavaScript");
+    let targetImage = document.querySelector("#smart-image");
+    targetImage.addEventListener("click", function() {
+    if (targetImage.classList.contains("small")) {
+    targetImage.classList.remove("small");
+} else {
+    targetImage.classList.add("small");
+}        
+});
 });
